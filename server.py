f7e37b2a70b81baecb3dd01837ad2fe35648e569
Max <patronsgg@yandex.ru>
2021-04-04 22:29:00 +0300

    Add first_page.html and upd server.py

--- server.py
+++ server.py
@@ -10,16 +10,11 @@ app.config['SECRET_KEY'] = 'abobaABOBAaboba'
 
 @app.route('/')
 def root():
-    return render_template('base.html')
-
-
-@app.route('/')
-def first_page():
-    return render_template('base.html')
+    return render_template('first_page.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
