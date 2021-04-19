2dbab20c3d388d71eca9d621461fd04a63ce08ed
Egor Belousov <egor0288354@yandex.ru>
2021-04-19 16:51:18 +0300

    Fix get_money func

--- get_money.py
+++ get_money.py
@@ -1,13 +1,13 @@
 import threading
 from data import db_session
-from data.users import User
+from data.players import Player
 
 
 def get_money():
     db_sess = db_session.create_session()
-    army = [(x, sum((item.number for item in x.player.army))) for x in db_sess.query(User).all()]
+    army = [(x, sum(item.number for item in x.army)) for x in db_sess.query(Player).all()]
     for x in army:
-        x[0].player.money += x[1]
+        x[0].money += x[1]
     db_sess.commit()
     threading.Timer(60, get_money).start()
 
