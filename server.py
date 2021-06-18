d8ea882c99dcfadf24097d43503b2b1b094ef808
Max <patronsgg@yandex.ru>
2021-06-18 17:53:13 +0300

    return full temp

--- server.py
+++ server.py
@@ -321,7 +321,39 @@ def temp():
         if diff > 0:
             message += 'Рейд прошел успешно! Вы получили часть денег и армии оппонента.'
             raid_.is_success = True
-            
+            withdraw_money = round(defender.money * uniform(0.25, 0.45))
+            raid_.withdraw_money = withdraw_money
+            prize_army = choice([x for x in army_defender])
+            prize_army_number = ceil(uniform(0.1, 0.2) * prize_army.number)
+            losses = raid_losses(attacker, defender)
+            raid_.losses_att, raid_.losses_def = sum(losses[0]), sum(losses[1])
+            attacker.money += withdraw_money
+            defender.money -= withdraw_money
+            if prize_army.race_id not in [x.race_id for x in army_attacker]:
+                prize = Army(
+                    player_id=attacker.id,
+                    race_id=prize_army.race_id,
+                    number=prize_army_number,
+                    level=1
+                )
+                db_sess.add(prize)
+                message += f' Вы открыли новую расу - {prize_army.race.title}!'
+            else:
+                army_to_add = list(filter(lambda x: x.race_id == prize_army.race_id, army_attacker))[0]
+                army_to_add.number += prize_army_number
+            raid_.prize_race_title = prize_army.race.title
+            raid_.prize_race_number = prize_army_number
+            defender.last_defend = datetime.now()
+        else:
+            raid_.is_success = False
+            losses = raid_losses(defender, attacker)
+            raid_.losses_att, raid_.losses_def = sum(losses[1]), sum(losses[0])
+            message += 'Рейд завершился неудачей. Вы потеряли часть своей армии.'
+        db_sess.add(raid_)
+        db_sess.commit()
+        return render_template(
+            'raid.html', raidform=raidform, message=message, raids=(attack_raids, defense_raids))
+    return render_template('raid.html', raidform=raidform, raids=(attack_raids, defense_raids))
 
 
 if __name__ == '__main__':
