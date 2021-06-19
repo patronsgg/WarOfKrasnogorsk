082fad74c75d14eb53855c7a8fd7a2b502139251
Max <patronsgg@yandex.ru>
2021-06-19 12:39:54 +0300

    edit ab.py

--- ab.py
+++ ab.py
@@ -1,36 +1,4 @@
-@app.route('/raid', methods=['GET', 'POST'])
-@login_required
-def raid():
-    db_sess = db_session.create_session()
-    available_users = []
-    for user in db_sess.query(User).all():
-        if user.id == current_user.id or not user.player:
-            continue
-        if not check_time(user.player.last_defend):
-            continue
-        available_users.append(
-            (user.username, f'{user.username} (мощь армии: {get_full_army_power(user.player.army)})')
-        )
-    raidform = RaidForm()
-    raidform.users.choices = available_users
-    player = db_sess.query(Player).get(current_user.id)
-    attack_raids = db_sess.query(Raid).filter(Raid.attacker_id == player.id)
-    defense_raids = db_sess.query(Raid).filter(Raid.defender_id == player.id)
-    if raidform.validate_on_submit():
-        raid_ = Raid()
-        message = ''
-        username = raidform.users.data
-        attacker = player
-        defender = db_sess.query(User).filter(User.username == username).first().player
-        raid_.attacker_id, raid_.defender_id = attacker.id, defender.id
-        army_attacker = attacker.army
-        army_defender = defender.army
-        total_attack = sum([get_stats_with_upgrades(x)[0] for x in army_attacker])
-        total_defense = sum([get_stats_with_upgrades(x)[1] for x in army_defender])
-        chance_attacker, chance_defender = uniform(0.75, 1.0), uniform(0.75, 1.0)
-        diff = round(total_attack * chance_attacker - total_defense * chance_defender)
-        if diff > 0:
-            message += 'Рейд прошел успешно! Вы получили часть денег и армии оппонента.'
+
             raid_.is_success = True
             withdraw_money = round(defender.money * uniform(0.25, 0.45))
             raid_.withdraw_money = withdraw_money
